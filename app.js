462b21364b9c2035b094b47ffd87d6f84a4abf63
hairunisa29 <hairunisa2909@outlook.com>
2023-07-06 10:44:02 +0700

    add modal create category, show data category in table, fix error import mongoose in models, connect to db mongoose

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+// import mongoose
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/db-staycation');
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 

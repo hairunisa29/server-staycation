14f03d56d67a18572c1a02b893caa4b216750a7e
hairunisa29 <hairunisa2909@outlook.com>
2023-07-07 16:36:29 +0700

    show image item

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -179,6 +179,7 @@ module.exports = {
         category,
         item,
         alert,
+        action: "view",
       });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
@@ -216,6 +217,35 @@ module.exports = {
         req.flash("alertStatus", "success");
         res.redirect("/admin/item");
       }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/item");
+    }
+  },
+
+  showImageItem: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Item.findOne({ _id: id }).populate({
+        path: "imageId",
+        select: "id imageUrl",
+      });
+
+      console.log(item.imageId);
+
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = {
+        message: alertMessage,
+        status: alertStatus,
+      };
+      res.render("admin/item/view_item", {
+        title: "Staycation | Show Image Item",
+        item,
+        alert,
+        action: "show image",
+      });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");

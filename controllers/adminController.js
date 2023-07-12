b8c7993f0d68533eaa717a9683391c677271efb5
hairunisa29 <hairunisa2909@outlook.com>
2023-07-12 10:26:26 +0700

    show detail booking

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -626,9 +626,26 @@ module.exports = {
       const booking = await Booking.find()
         .populate("memberId")
         .populate("bankId");
-      console.log(booking);
+
       res.render("admin/booking/view_booking", {
         title: "Staycation | Booking",
+        user: req.session.user,
+        booking,
+      });
+    } catch (error) {
+      res.redirect("/admin/booking");
+    }
+  },
+
+  showDetailBooking: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const booking = await Booking.findOne({ _id: id })
+        .populate("memberId")
+        .populate("bankId");
+      
+      res.render("admin/booking/show_detail_booking", {
+        title: "Staycation | Detail Booking",
         user: req.session.user,
         booking,
       });
--- routes/admin.js
+++ routes/admin.js
@@ -7,17 +7,21 @@ router.get("/signin", adminController.viewSignIn);
 router.post("/signin", adminController.actionSignIn);
 router.use(auth);
 router.get("/logout", adminController.actionLogout);
+
 router.get("/dashboard", adminController.viewDashboard);
+
 // endpoint category
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
+
 // endpoint bank
 router.get("/bank", adminController.viewBank);
 router.post("/bank", uploadSingle, adminController.addBank);
 router.put("/bank", uploadSingle, adminController.editBank);
 router.delete("/bank/:id", adminController.deleteBank);
+
 // endpoint item
 router.get("/item", adminController.viewItem);
 router.post("/item", uploadMultiple, adminController.addItem);
@@ -25,6 +29,7 @@ router.get("/item/show-image/:id", adminController.showImageItem);
 router.get("/item/:id", adminController.showEditItem);
 router.put("/item/:id", uploadMultiple, adminController.editItem);
 router.delete("/item/:id/delete", adminController.deleteItem);
+
 // endpoint detail item
 router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
 router.post("/item/add/feature", uploadSingle, adminController.addFeature);
@@ -35,6 +40,8 @@ router.post("/item/add/activity", uploadSingle, adminController.addActivity);
 router.put("/item/update/activity", uploadSingle, adminController.editActivity);
 router.delete("/item/:itemId/activity/:id", adminController.deleteActivity);
 
+// endpoint booking
 router.get("/booking", adminController.viewBooking);
+router.get("/booking/:id", adminController.showDetailBooking);
 
 module.exports = router;
